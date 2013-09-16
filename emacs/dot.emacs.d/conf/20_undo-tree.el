ff6bab57214e3f944049129036e1fa8a564edb28
Shigeki Morimoto <Shigeki@Morimo.to>
2013-09-16 19:59:00 +0900

    add config for undo-tree.

---
+++ emacs/dot.emacs.d/conf/20_undo-tree.el
@@ -0,0 +1,9 @@
+;;
+;; C-x u    undo-tree-visualize
+;;
+;; C-f, C-b    undo-tree-visualizer-select-left/right
+;; C-p, C-n    undo-tree-visualizer-select-previous/next
+;;
+
+(el-get 'sync 'undo-tree)
+(global-undo-tree-mode)
