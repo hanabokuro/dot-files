4081a2ebccf2b8381b7eda43f4e5f670b814ce8a
Shigeki Morimoto <Shigeki@Morimo.to>
2013-09-17 01:12:03 +0900

    add my-helm-source-calculation-result (quick calculator).

--- emacs/dot.emacs.d/conf/20_helm.el
+++ emacs/dot.emacs.d/conf/20_helm.el
@@ -6,16 +6,17 @@
 ;; M-x helm-locate
 ;;
 ;;
+;; @helm-find-files
+;; C-. or C-l         go up directory
+;; C-z                select directory
+;; C-c ?              show help
+;;
 
 (el-get 'sync 'helm)
 (require 'helm-config)
 
-(global-set-key "\C-xb" 'helm-mini)
 (helm-mode 1)
-
-(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
-
- (define-key helm-map (kbd "C-i") 'undefined)
+; (define-key helm-map (kbd "C-i") 'undefined)
 
 (add-hook 'eshell-mode-hook
           #'(lambda ()
@@ -24,4 +25,31 @@
                 'helm-esh-pcomplete)))
 
 (global-set-key "\M-\C-y" 'helm-show-kill-ring)
-(global-set-key "\C-c\C-f" 'helm-find-files)
+
+(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
+(global-set-key "\C-c\C-f" ' helm-find-files)
+
+
+(defvar my-helm-source-calculation-result
+  '((name . "Calculation Result")
+    (dummy)
+    (type . 'string)
+    (filtered-candidate-transformer . (lambda (candidates source)
+                                        (list
+                                         (condition-case nil
+                                             (with-helm-current-buffer (calc-eval helm-pattern))
+                                           (error "Error")))))
+    (action . (("Copy result to kill-ring" . kill-new)))
+))
+
+(global-set-key "\C-xb" '(lambda ()
+                           (interactive)
+                           (require 'helm-files)
+                           (helm-other-buffer '(
+                                                helm-source-buffers-list
+                                                helm-source-recentf
+                                                helm-source-buffer-not-found
+                                                my-helm-source-calculation-result
+                                                )
+                                              "*helm mini*")))
+
