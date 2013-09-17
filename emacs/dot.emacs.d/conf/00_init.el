4b18f55999bbb3f6407c0a9c2e2058847140a67b
Shigeki Morimoto <github@morimo.to>
2013-09-17 19:32:59 +0900

    remove -i from set-exec-path-from-shell-PATH.

--- emacs/dot.emacs.d/conf/00_init.el
+++ emacs/dot.emacs.d/conf/00_init.el
@@ -23,7 +23,7 @@
 
 This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
   (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 (set-exec-path-from-shell-PATH)
