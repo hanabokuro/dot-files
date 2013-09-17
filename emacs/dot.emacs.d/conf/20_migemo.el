8300393051b9d02d15901c5a1d41037278b7e32f
Shigeki Morimoto <github@morimo.to>
2013-09-17 19:33:43 +0900

    use ~/local/share/migemo if exists.

--- emacs/dot.emacs.d/conf/20_migemo.el
+++ emacs/dot.emacs.d/conf/20_migemo.el
@@ -7,7 +7,8 @@
 
 (setq migemo-command "cmigemo")
 (setq migemo-options '("-q" "--emacs"))
-(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(and (file-exists-p "/usr/local/share/migemo/utf-8/migemo-dict") (setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"))
+(and (file-exists-p "~/local/share/migemo/utf-8/migemo-dict")    (setq migemo-dictionary (expand-file-name "~/local/share/migemo/utf-8/migemo-dict")))
 (setq migemo-user-dictionary nil)
 (setq migemo-regex-dictionary nil)
 (setq migemo-coding-system 'utf-8-unix)
