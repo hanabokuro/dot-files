6bda681e0c8d255f735c928e29c8e84898f44590
Shigeki Morimoto <github@morimo.to>
2013-09-18 17:51:49 +0900

    add comment to cua-mode.

--- emacs/dot.emacs.d/conf/20_cua-mode.el
+++ emacs/dot.emacs.d/conf/20_cua-mode.el
@@ -1,3 +1,9 @@
 ;; regionを選択したあと C-<RET> で矩形選択モード
+;;
+;; M-b    fill with space
+;; M-i    increment numbers
+;; M-k    kill rectangle
+;; M-n    like a seq command
+;;
 (cua-mode t)
 (setq cua-enable-cua-keys nil)
