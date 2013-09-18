ea873bdb9619376b91a49178e00ca87dd42d43d0
Shigeki Morimoto <github@morimo.to>
2013-09-18 17:53:06 +0900

    edit auto_complete config.

--- emacs/dot.emacs.d/conf/20_auto-complete.el
+++ emacs/dot.emacs.d/conf/20_auto-complete.el
@@ -2,5 +2,8 @@
 
 (setq ac-menu-height 40)
 (setq ac-use-menu-map t)
+
+(require 'auto-complete-config)
+(ac-config-default)
 ;(define-key ac-menu-map "\C-n" 'ac-next)
 ;(define-key ac-menu-map "\C-p" 'ac-previous)
