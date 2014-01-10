b5709cb6dd1e1e0caf6ff93b3eee58b4227e639c
Shigeki Morimoto <Shigeki@Morimo.to>
2014-01-10 17:18:08 +0900

    Add inf-ruby-console-padrino.

--- emacs/dot.emacs.d/conf/40_ruby.el
+++ emacs/dot.emacs.d/conf/40_ruby.el
@@ -1,2 +1,19 @@
 (el-get 'sync 'inf-ruby)
 (el-get 'sync 'ruby-mode)
+
+(require 'inf-ruby)
+
+(add-to-list 'inf-ruby-console-patterns-alist '("config/apps.rb" . padrino))
+
+(defun inf-ruby-console-padrino (dir)
+  "Run Padrino console in DIR."
+  (interactive "D")
+  (let ((default-directory dir))
+    (run-ruby "bundle exec padrino console" "padrino")))
+
+;;
+;; Add them to ~/.hirb.yml to disable warning "WARNING: terminal is not fully functional"
+;; ---
+;; :pager: false
+;;
+
