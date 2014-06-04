cf3eabe8c95f63f8c2d0fad0b62a7f79ff34d53e
shigeki.morimoto <shigeki.morimoto@20201275n.intra.emercury.co.jp>
2014-06-04 11:47:30 +0900

    change region color

--- emacs/dot.emacs.d/conf/20_color-theme.el
+++ emacs/dot.emacs.d/conf/20_color-theme.el
@@ -36,3 +36,11 @@
 
 (el-get 'sync 'darkburn-theme)
 (load "darkburn-theme")
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "purple1")))))
+ )
--- emacs/dot.emacs.d/init.el
+++ emacs/dot.emacs.d/init.el
@@ -61,12 +61,7 @@
  '(eshell-stringify-t nil)
  '(eshell-term-name "ansi")
  '(eshell-visual-commands (quote ("vi" "vim" "top" "screen" "less" "lynx" "ssh" "rlogin" "telnet")))
+ '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(font-lock-maximum-decoration t)
  '(safe-local-variable-values (quote ((encoding . utf-8))))
  '(show-paren-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
