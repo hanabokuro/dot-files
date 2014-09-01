bf7bc1d3b005e27cd22413115d834c6bb68ebf29
shigeki morimoto <shmorimo@shigeki-no-MacBook-Pro.local>
2014-09-01 18:24:03 +0900

    add bash-completion.el

--- emacs/dot.emacs.d/conf/30_shell.el
+++ emacs/dot.emacs.d/conf/30_shell.el
@@ -1,5 +1,10 @@
 (add-hook 'shell-mode-hook  '(lambda () (define-key shell-mode-map "\C-c\C-f" 'helm-find-files)))
 
+(el-get 'sync 'bash-completion)
+
+(require 'bash-completion)
+(bash-completion-setup)
+
 
 
 
