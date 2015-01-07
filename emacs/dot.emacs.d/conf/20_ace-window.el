16dbd4577e924045090e2306c8ad0b054fc01429
shigeki morimoto <github@morimo.to>
2015-01-07 16:22:17 +0900

    disable ace-window

--- emacs/dot.emacs.d/conf/20_ace-window.el
+++ emacs/dot.emacs.d/conf/20_ace-window.el
@@ -1,3 +1,3 @@
-(el-get 'sync 'ace-window)
-(global-set-key (kbd "C-x o") 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+; (el-get 'sync 'ace-window)
+; (global-set-key (kbd "C-x o") 'ace-window)
+; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
