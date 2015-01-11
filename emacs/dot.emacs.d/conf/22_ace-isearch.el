7d556082aed224a856da48f990c593ab7d4c6600
Shigeki Morimoto <Shigeki@Morimo.to>
2015-01-11 14:31:18 +0900

    Add some mode, rcp, etc

--- emacs/dot.emacs.d/conf/22_ace-isearch.el
+++ emacs/dot.emacs.d/conf/22_ace-isearch.el
@@ -1,6 +1,5 @@
-; (el-get 'sync 'ace-isearch)
-; (package-install 'ace-isearch)
-
+(el-get 'sync 'helm-swoop)
+(el-get 'sync 'ace-isearch)
 
 (global-ace-isearch-mode 1)
 (setq ace-isearch-input-length 1024)
