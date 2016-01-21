64112589a19ca9abd8e6ba6dac8587ddb42f1cd8
Shigeki Morimoto <github@morimo.to>
2016-01-21 12:45:30 +0900

    ediff

--- emacs/dot.emacs.d/conf/20_ediff.el
+++ emacs/dot.emacs.d/conf/20_ediff.el
@@ -1,1 +1,6 @@
 (setq ediff-grab-mouse nil)
+
+;; コントロール用のバッファを同一フレーム内に表示
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; diffのバッファを上下ではなく左右に並べる
+(setq ediff-split-window-function 'split-window-horizontally)
