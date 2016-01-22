4bb802931e3c27ffff82c426905ff6320098e902
Shigeki Morimoto <github@morimo.to>
2016-01-22 16:43:15 +0900

    Use helm-migemo & at helm-swoop

--- emacs/dot.emacs.d/conf/20_helm-swoop.el
+++ emacs/dot.emacs.d/conf/20_helm-swoop.el
@@ -1,5 +1,6 @@
 ;;
 ;; C-s  then  M-i
+;; C-x C b    helm-resume
 ;;
 
 (el-get 'sync 'helm-swoop)
--- emacs/dot.emacs.d/conf/21_helm-migemo.el
+++ emacs/dot.emacs.d/conf/21_helm-migemo.el
@@ -1,5 +1,4 @@
 (el-get 'sync 'helm-migemo)
 (require 'helm-migemo)
-
+(helm-migemo-mode 1)
 (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
-
